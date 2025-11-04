64a85dd803a87003fa789a3c419ba40808a4721f
Gabryel Siqueira Navarro <gabryelsn2001@gmail.com>
2025-11-04 10:57:40 -0300

    Alura Space Atualizado

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-
 def index(request):
-   return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
-   return render (request, 'galeria/imagem.html')
+    return render(request, 'galeria/imagem.html')
