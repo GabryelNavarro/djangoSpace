e99ac226e27ada1245c28f0c8d7d00a5e7ada148
Gabryel Siqueira Navarro <gabryelsn2001@gmail.com>
2025-11-12 13:14:27 -0300

    Instalando dependencias

--- galeria/models.py
+++ galeria/models.py
@@ -16,6 +16,7 @@ class Fotografia(models.Model):
     categoria = models.CharField(max_length=100, default='', choices=OPCOES_CATEGORIA)
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
+    publicado = models.BooleanField(default=False)
 
 
     def __str__(self):
