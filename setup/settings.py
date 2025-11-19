e3832a6458368eec42032a9eac68f20dd1195929
Gabryel Siqueira Navarro <gabryelsn2001@gmail.com>
2025-11-19 11:57:20 -0300

    Formulario de Login

--- setup/settings.py
+++ setup/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galeria.apps.GaleriaConfig',
-    'rangefilter'
+    'rangefilter',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
---
+++ usuarios/form.py
@@ -0,0 +1,77 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label= "Nome de Usuário",
+        required=True,
+        max_length=100,
+         widget= forms.TextInput(
+            attrs={
+                
+                "placeholder":"Nome Login"
+            }
+        )
+    )
+    senha=forms.CharField(
+        label= "Senha",
+        required=True,
+        max_length=70,
+        widget= forms.PasswordInput(
+            attrs={
+               
+                "placeholder":"Digite sua senha"
+            }
+
+        )
+    )
+
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label= "nome completo",
+        required=True,
+        max_length=100,
+         widget= forms.TextInput(
+            attrs={
+                
+                "placeholder":"Nome de Usuário"
+            }
+        )
+    )
+    email_cadastro = forms.EmailField(
+         label= "nome completo",
+        required=True,
+        max_length=100,
+         widget= forms.EmailInput(
+            attrs={
+                
+                "placeholder":"Insira seu E-mail para cadastrar-se em nosso site"
+            }
+        )
+    )
+    senha_cadastro_1=forms.CharField(
+        label= "Senha",
+        required=True,
+        max_length=70,
+        widget= forms.PasswordInput(
+            attrs={
+               
+                "placeholder":"Digite sua senha escolhida"
+            }
+
+        )
+    )
+    senha_cadastro_2=forms.CharField(
+        label= "Senha",
+        required=True,
+        max_length=70,
+        widget= forms.PasswordInput(
+            attrs={
+               
+                "placeholder":"Digite Novamente a senha escolhida"
+            }
+
+        )
+    )
+    
